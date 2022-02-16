3ccee1ff70f1766654343fee0ea4f1e40181c33c
Juel Hossain <juelhossainnn@gmail.com>
2022-02-17 04:29:11 +0600

    main part calculation done

--- master.js
+++ master.js
@@ -1,17 +1,54 @@
-const $ = document.querySelector();
+//making document.querySelector shorter with the help of function.
+function $(selector) { return document.querySelector(selector) } 
+//getting all important element at first.
+// main section 
+// input
 const incomeInput = $("#income");
 const foodInput = $("#food");
 const rentInput = $("#rent");
 const clothesInput = $("#clothes");
 const calculateButton = $("#calculate");
-const totalExpenses = $("#expenses");
+
+// output 
+const expenses = $("#expenses");
 const balances = $("#balances");
+
+//save section
+// input 
 const saveInput = $("#save");
 const saveButton = $("#save-btn");
+// output 
 const savingAmount = $("#saving-amount");
 const remainingBalances = $("#remaining-balances");
+
+// errrorrr
 const errorIncome = $("#errorincome");
 const errorFood = $("#errorfood");
 const errorRent = $("#errorrent");
 const errorClothes = $("#errorclothes");
 const errorSave = $("#errorsave");
+
+
+
+
+calculateButton.addEventListener("click", function () {
+    //getting the values of expenses.
+let food = +foodInput.value;
+let rent = +rentInput.value;
+let clothes = +clothesInput.value;
+let income = +incomeInput.value;
+//calculating the expenses.
+let totalExpenses = food + rent + clothes;
+//now calucaling the remaining balances after cutting the expenses;
+    let remBalances = (income - totalExpenses);
+    // pushing result to the output
+    expenses.innerHTML = totalExpenses;
+    balances.innerHTML = remBalances;
+});
+
+saveButton.addEventListener("click", function () {
+    let save = +saveInput.value;
+    let income = +incomeInput.value;
+
+
+})
