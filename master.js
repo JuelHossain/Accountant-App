b5bbfea5a331b474b300e250b0f542bc6fb03c65
Juel Hossain <juelhossainnn@gmail.com>
2022-02-17 19:15:14 +0600

    error handling working

--- master.js
+++ master.js
@@ -1,54 +1,99 @@
 //making document.querySelector shorter with the help of function.
 function $(selector) { return document.querySelector(selector) } 
-//getting all important element at first.
-// main section 
-// input
+
+// main section input
 const incomeInput = $("#income");
 const foodInput = $("#food");
 const rentInput = $("#rent");
 const clothesInput = $("#clothes");
-const calculateButton = $("#calculate");
 
 // output 
 const expenses = $("#expenses");
 const balances = $("#balances");
 
-//save section
-// input 
+//save section input
 const saveInput = $("#save");
-const saveButton = $("#save-btn");
+
 // output 
 const savingAmount = $("#saving-amount");
-const remainingBalances = $("#remaining-balances");
+const remainingBalance = $("#remaining-balances");
 
-// errrorrr
+// buttons 
+const calculateButton = $("#calculate");
+const saveButton = $("#save-btn");
+
+// errrorrr handler
 const errorIncome = $("#errorincome");
 const errorFood = $("#errorfood");
 const errorRent = $("#errorrent");
 const errorClothes = $("#errorclothes");
 const errorSave = $("#errorsave");
 
-
-
+// this is the changes i want to do when the field is invalid.
 
 calculateButton.addEventListener("click", function () {
+    function showError(inputField, errorField, errorMessage) {
+        inputField.style.border ='1px solid red'
+        errorField.style.display = 'inline';
+        errorField.innerText = errorMessage;
+}
+// this is the changes i want to remove when field is valid.
+function removeError(inputField, errorField) {
+        inputField.style.border ='1px solid black'
+        errorField.style.display = 'none';
+        errorField.innerText = ""
+}
+function totalExpenses() {
     //getting the values of expenses.
-let food = +foodInput.value;
-let rent = +rentInput.value;
-let clothes = +clothesInput.value;
-let income = +incomeInput.value;
-//calculating the expenses.
-let totalExpenses = food + rent + clothes;
-//now calucaling the remaining balances after cutting the expenses;
-    let remBalances = (income - totalExpenses);
-    // pushing result to the output
-    expenses.innerHTML = totalExpenses;
-    balances.innerHTML = remBalances;
+    let food = +foodInput.value;
+    let rent = +rentInput.value;
+    let clothes = +clothesInput.value;
+    
+    if (food < 0 || rent < 0 || clothes < 0) {
+        let value = 0;
+        if (food < 0) {
+            showError(foodInput, errorFood, 'no food expenses')
+        } if (rent < 0) {
+            showError(rentInput, errorRent, 'no rent has been given');
+        } if (clothes < 0) {
+            showError(clothesInput, errorClothes, 'no clothes have been bought')
+        }
+        return 0;
+    }else {
+        return food + rent + clothes;
+    }
+}
+function rembalances() {
+    let income = +incomeInput.value;
+    incomeInput.addEventListener('input', function () {
+        removeError(incomeInput, errorIncome);
+    })
+    if (income == "") {
+        return showError(incomeInput,errorIncome,'You must fill this field')
+    } else if (income < 0) {
+        return showError(incomeInput,errorIncome,"You don't have enough income")
+    } else {
+        return income - totalExpenses();
+    }
+}
+    if (errorIncome=== 'inline') {
+        balances.innerText = 'something is wrong!'
+    } else {
+        expenses.innerHTML = totalExpenses();
+        balances.innerHTML = rembalances();
+    }
 });
 
-saveButton.addEventListener("click", function () {
+function totalSavings() {
     let save = +saveInput.value;
     let income = +incomeInput.value;
+    return (income / 100) * save;
+};
+function remainingBalances() {
+    return rembalances() - totalSavings();
+}
 
-
-})
+saveButton.addEventListener("click", function () {
+    savingAmount.innerHTML = totalSavings();
+    remainingBalance.innerHTML = remainingBalances();
+});
