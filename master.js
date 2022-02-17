1a6e59b6f0b526e126ce3ea10f4c1019e3c324df
Juel Hossain <juelhossainnn@gmail.com>
2022-02-17 21:45:25 +0600

    javascript part done

--- master.js
+++ master.js
@@ -8,15 +8,15 @@ const rentInput = $("#rent");
 const clothesInput = $("#clothes");
 
 // output 
-const expenses = $("#expenses");
-const balances = $("#balances");
+const expensesOutput = $("#expenses");
+const balancesOutput = $("#balances");
 
 //save section input
 const saveInput = $("#save");
 
 // output 
-const savingAmount = $("#saving-amount");
-const remainingBalance = $("#remaining-balances");
+const savingAmountOutput = $("#saving-amount");
+const remainingBalanceOutput = $("#remaining-balances");
 
 // buttons 
 const calculateButton = $("#calculate");
@@ -29,10 +29,23 @@ const errorRent = $("#errorrent");
 const errorClothes = $("#errorclothes");
 const errorSave = $("#errorsave");
 
+function income() {
+    return +incomeInput.value;
+}
+function expenses() {
+    let food = +foodInput.value;
+    let rent = +rentInput.value;
+    let clothes = +clothesInput.value;
+    return food + rent + clothes;
+}
+function balances() {
+    return income() - expenses();
+}
+function output(field,inner) {
+    return field.innerText = inner;
+}
 // this is the changes i want to do when the field is invalid.
-
-calculateButton.addEventListener("click", function () {
-    function showError(inputField, errorField, errorMessage) {
+function showError(inputField, errorField, errorMessage) {
         inputField.style.border ='1px solid red'
         errorField.style.display = 'inline';
         errorField.innerText = errorMessage;
@@ -43,57 +56,85 @@ function removeError(inputField, errorField) {
         errorField.style.display = 'none';
         errorField.innerText = ""
 }
-function totalExpenses() {
-    //getting the values of expenses.
-    let food = +foodInput.value;
-    let rent = +rentInput.value;
-    let clothes = +clothesInput.value;
-    
-    if (food < 0 || rent < 0 || clothes < 0) {
-        let value = 0;
-        if (food < 0) {
-            showError(foodInput, errorFood, 'no food expenses')
-        } if (rent < 0) {
-            showError(rentInput, errorRent, 'no rent has been given');
-        } if (clothes < 0) {
-            showError(clothesInput, errorClothes, 'no clothes have been bought')
-        }
-        return 0;
-    }else {
-        return food + rent + clothes;
-    }
-}
-function rembalances() {
-    let income = +incomeInput.value;
-    incomeInput.addEventListener('input', function () {
+incomeInput.addEventListener('input', function () {
+    if (incomeInput.value < 0) {
+        showError(incomeInput, errorIncome, "You don't have enough income");
+        incomeInput.value = "";
+    } else {
         removeError(incomeInput, errorIncome);
-    })
-    if (income == "") {
-        return showError(incomeInput,errorIncome,'You must fill this field')
-    } else if (income < 0) {
-        return showError(incomeInput,errorIncome,"You don't have enough income")
-    } else {
-        return income - totalExpenses();
-    }
-}
-    if (errorIncome=== 'inline') {
-        balances.innerText = 'something is wrong!'
-    } else {
-        expenses.innerHTML = totalExpenses();
-        balances.innerHTML = rembalances();
     }
 });
+foodInput.addEventListener('input', function () {
+        if (foodInput.value < 0) {
+            showError(foodInput, errorFood, 'no food expenses');
+            foodInput.value = "";
+        } else {
+            removeError(foodInput, errorFood);
+        }
+    });
+rentInput.addEventListener('input', function () {
+            if (rentInput.value < 0) {
+                showError(rentInput, errorRent, 'no rent has been given');
+                rentInput.value = "";
+            } else {
+                removeError(rentInput, errorRent);
+            }
+        });
+clothesInput.addEventListener('input', function () {
+            if (clothesInput.value < 0) {
+                showError(clothesInput, errorClothes, 'no rent has been given');
+                clothesInput.value = "";
+            } else {
+                removeError(clothesInput, errorClothes);
+            }
+});
+saveInput.addEventListener('input', function () {
+    if (saveInput.value < 0) {
+        showError(saveInput, errorSave, 'invalid');
+        saveInput.value = "";
+    } else {
+        removeError(saveInput, errorSave);
+            }
+        })
 
-function totalSavings() {
+calculateButton.addEventListener("click", function () {
+
+    if (income() == "") {
+        showError(incomeInput, errorIncome, "This field cannot be empty");
+        output(expensesOutput, "");
+        output(balancesOutput, "");
+    } else if (expenses() > income()) {
+        showError(incomeInput, errorIncome, "Not Enough Income");
+        output(expensesOutput, "");
+        output(balancesOutput, "");
+    } else {
+        output(balancesOutput, balances());
+        output(expensesOutput, expenses());
+    }
+});
+function savingAmount() {
     let save = +saveInput.value;
     let income = +incomeInput.value;
     return (income / 100) * save;
-};
-function remainingBalances() {
-    return rembalances() - totalSavings();
 }
-
+function remainingBalances() {
+    return balances() - savingAmount();
+}
 saveButton.addEventListener("click", function () {
-    savingAmount.innerHTML = totalSavings();
-    remainingBalance.innerHTML = remainingBalances();
+    if (income() == "") {
+        showError(saveInput, errorSave, "not Enough Income");
+        output(savingAmountOutput, "");
+        output(remainingBalanceOutput, "");
+    } else if (saveInput.value == "") {
+        showError(saveInput, errorSave, "need percentage");
+        output(savingAmountOutput, "");
+        output(remainingBalanceOutput, "");
+    }else if (savingAmount()>balances()) {
+        showError(saveInput, errorSave, "not Enough Balances");
+        output(savingAmountOutput, "");
+        output(remainingBalanceOutput, "");
+    } else {
+        output(savingAmountOutput, savingAmount());
+        output(remainingBalanceOutput, remainingBalances());
+    }
 });
